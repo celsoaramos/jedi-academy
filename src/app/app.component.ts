d1663488df99fbcc0c096a5588944dd666555233
celsoaramos <celsoaramos@gmail.com>
2018-06-02 16:57:44 -0300

    31. Lidando com Erros

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import { Student } from './student/student.model';
+
 
 @Component({
   selector: 'jad-root',
   templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'jad works2!';
+
+  students: Student[] = [
+    {name: 'Luke', isJedi: true, temple: 'Coruscant'},
+    {name: 'Leia', isJedi: false},
+    {name: 'han solo', isJedi: false}
+  ]
 }
